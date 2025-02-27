04497e1f362e58cf95704614c736a6710bd9d847
Blusted <86549717+Blxsted@users.noreply.github.com>
2025-02-27 07:25:29 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,57 @@
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = '!<>-_\\/[]{}—=+*^?#________';
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise(resolve => this.resolve = resolve);
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = '';
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const faqItems = document.querySelectorAll(".faq-item");
   faqItems.forEach(item => {
@@ -11,4 +65,8 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.from(".faq-section", { y: 50, opacity: 0, duration: 1, delay: 1.1 });
 
   ScrollReveal().reveal('.hero', { duration: 1500, origin: 'bottom', distance: '50px' });
+
+  const el = document.querySelector('.scramble-text');
+  const fx = new TextScramble(el);
+  fx.setText("El Bot Definitivo para Discord");
 });
