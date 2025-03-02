2ce7fadd6a2ca48a0fa2e14e7ddaf6edf05b4792
Adrian <86549717+4drixn@users.noreply.github.com>
2025-03-01 19:36:49 -0600

    Update panel.js

--- js/panel.js
+++ js/panel.js
@@ -1,5 +1,5 @@
 document.getElementById("loginBtn").addEventListener("click", function() {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1342201886727475200&redirect_uri=https://www.hazybot.net/panel.html&response_type=code&scope=identify";
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=1342201886727475200&redirect_uri=https://www.hazybot.net/panel.html&response_type=code&scope=identify";
 });
 
 document.getElementById("sendMessage").addEventListener("click", function() {
